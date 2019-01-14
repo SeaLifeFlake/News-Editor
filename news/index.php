d3d42837ae026341222645921b3dd2f3498ca924
Fabian Schuss <34821584+FabianGhost@users.noreply.github.com>
2019-01-14 21:28:26 +0100

    Update index.php

--- news/index.php
+++ news/index.php
@@ -10,15 +10,15 @@
   $inhalt = fgets($afile); //DEFINE YOUR CONTENTS 
   fclose($afile);
 ?>
-//if you WANT ADD SOME CSS HERE
+<!-- if you WANT ADD SOME CSS HERE -->
 <link type="text/css" rel="stylesheet" href="/PATH/TO/style.css">
 
-<head>
-  <title><?php echo $version?></title> //FOR YOUR VERSION VARIABLE
-</head>
+ <head>
+  <title><?php echo $version?></title> <!-- FOR YOUR VERSION VARIABLE -->
+ </head>
 
-<body id="SOME_ID_OR_DELETE"> //ADD SOME CSS ID OR CLASS IF YOU WANT
-<p><b>News editor</b></p> //TILE
+ <body id="SOME_ID_OR_DELETE"> <!-- ADD SOME CSS ID OR CLASS IF YOU WANT -->
+  <p><b>News editor</b></p> //TILE
   <form action="edit.php" method="post">
     Update your News!<br>
     <textarea type="text" name="news" cols='200'><?php echo $inhalt ?></textarea><br>
@@ -27,9 +27,8 @@
     <input type="submit" value="Update">
   </form>
   <br>
-  <p class="center"><a href="LINK_TO_YOUR_HOMEPAGE">Zurück zur Homepage(Klick)</a></p>
+  <p class="center"><a href="LINK_TO_YOUR_HOMEPAGE">Zurück zur Homepage(Klick)</a></p> <!-- Some Hyperlinks! -->
   <br>
-  <p style="color:red"><b><?php echo $version?></b></p>
-</body>
-
+  <p style="color:red"><b><?php echo $version?></b></p>  <!-- Displays your Version -->
+ </body>
 </html>
