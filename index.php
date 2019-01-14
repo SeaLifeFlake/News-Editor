30f7427387dcb9ef49cec173756aea82782eb7cd
Fabian Schuss <34821584+FabianGhost@users.noreply.github.com>
2019-01-14 21:42:43 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="DE">
+	<head>
+  
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>YOUR_TITLE</title>
+    
+	</head>
+  <body>
+  
+		<marquee style="font-size: 15pt">
+			<?php
+				$myfile = fopen('PATH/TO/YOUR_NEWSFILE.txt', 'r') or die('Something goes WRONG!');   <!-- YOUR NEWS WILL BE DISPLAYED HERE -->
+				echo fgets($myfile);
+				fclose($myfile);
+			?>
+		</marquee>
+    
+   </body>
+</html>
